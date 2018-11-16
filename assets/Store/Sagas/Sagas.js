ba0ac050cda67e21eb7dbe290855f14a325a3841
jpknap <jp.knap@gmail.com>
2018-11-16 15:39:24 -0300

    instalacion de saga

---
+++ assets/Store/Sagas/Sagas.js
@@ -0,0 +1,4 @@
+export default function* funcionPrimaria() {
+  // permite usar la palabra reservada yield
+  console.log('Desde  funcion generadora ');
+}
--- assets/Store/Store.js
+++ assets/Store/Store.js
@@ -1,5 +1,8 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { reducer as form } from 'redux-form';
+import createSagaMiddleware from 'redux-saga'
+import funcionPrimaria from './Sagas/Sagas'
+
 
 // Funcion reducer
 const reducerDemo = (state = [0], action) => {
@@ -10,23 +13,17 @@ const reducerDemo = (state = [0], action) => {
       return state;
   }
 };
-// curring function
-const miMiddlewareDemo = store => next => (action) => {
-  console.log('se ejecuta el middleware');
-};
 
-const lastMiddleware= store => next => (action) => {
-  console.log('ultimo middleware');
-  next(action);
-};
+const sagaMiddleware = createSagaMiddleware();
 
 // version simplificada el key del objeto queda como el nombre de la variable, intedra el store del form
 const reducers = combineReducers({
   reducerDemo,
   form,
 });
-
 // El store lo extrae del mismo reducer
-const Store = createStore(reducers, applyMiddleware(miMiddlewareDemo, lastMiddleware));
+const Store = createStore(reducers, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(funcionPrimaria);
 
 export default Store;
